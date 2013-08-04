1956e04cb958dd391296f726ff7945f3ab5c05a0
Fabricio Buzeto <nuk.anime13@gmail.com>
2013-08-04 16:13:33 -0400

    Some refactorings

--- src/main/java/Crivo.java
+++ src/main/java/Crivo.java
@@ -5,14 +5,16 @@ import java.util.List;
 public class Crivo {
 
 	public static List<Integer> primes(int limit) {
-		List<Integer> allNumber = createAllNumbersToTheLimit(limit);
+		return findNumbersThatAreNotDivisible(
+										createAllNumbersToTheLimit(limit));
+	}
+
+	private static List<Integer> findNumbersThatAreNotDivisible(List<Integer> allNumber) {
 		List<Integer> result = new ArrayList<Integer>();
 		
 		result.add(allNumber.get(0));
-		
 		for(int i = 1; i < allNumber.size(); i++){
 			int number = allNumber.get(i);
-			
 			if (!isDivisible(result, number)){
 				result.add(number);
 			}
@@ -24,12 +26,16 @@ public class Crivo {
 		boolean divisible = false;
 		for(int prime : result){
 			if (prime <= Math.sqrt(number)){
-				divisible |= number % prime == 0;
+				divisible |= isDivisible(number, prime);
 			}else{
-				break;
+				return divisible;
 			}
 		}
-		return divisible;
+		return false;
+	}
+
+	private static boolean isDivisible(int number, int prime) {
+		return number % prime == 0;
 	}
 
 	private static List<Integer> createAllNumbersToTheLimit(int limit) {
